818f6dc9b07c5c99ec1bc2cc740859092532f5ad
Tomasz Pach <tomaszpach@icloud.com>
2018-10-16 12:31:12 +0200

    add static directory and example header image

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './myStyles.scss';
 const App = () => {
     return (
         <div className="app">
+            <img src="/dist/images/header.jpg" alt="header" className="app-header"/>
             <p>
                 We are a most promising species, Mr. Spock, as predators go. Did you know that? I frequently
                 have my doubts. I dont. Not any more. And maybe in a thousand years or so, we will be able
