6dfc5fe98470adc1d8cb93b977301a040948f955
Tomasz Pach <tomaszpach@icloud.com>
2018-10-16 15:20:06 +0200

    code splitting - missing example component

---
+++ src/Foo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// You can use export default like this or...
+// export default () => {}
+const Foo = () => (
+    <div>
+        <p>I am Foo! Pleasure to meet you.</p>
+    </div>
+);
+
+// Like this
+export default Foo;
