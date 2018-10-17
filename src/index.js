3d9a3e228736eecfc1b993b7a3d58e0afe2ad749
Tomasz Pach <tomaszpach@icloud.com>
2018-10-17 09:57:49 +0200

    prepare simple board

--- src/index.js
+++ src/index.js
@@ -1,49 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import initialData from './initial-data';
 
 import './myStyles.scss';
 
 class App extends React.Component {
     state = {
-        CaptainKirkBio: {},
+        initialData,
         Board: null, // Board is out component
     };
 
     componentDidMount() {
-        this.onGetKirkBio();
         import(/* webpackChunkName: 'Board' */ './Board').then(Board => {
             this.setState({ Board: Board.default });
         })
     }
 
-    onGetKirkBio = async () => {
-        try {
-            const result = await fetch('http://stapi.co/api/v1/rest/character/search', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                body: {
-                    title: 'James T. Kirk',
-                    name: 'James T. Kirk',
-                },
-            });
-
-            const resultJSON = await result.json();
-            const character = resultJSON.characters[0];
-            this.setState({ CaptainKirkBio: character });
-        } catch (error) {
-            console.log('error: ', error);
-        }
-    };
-
     render() {
-        const { CaptainKirkBio, Board } = this.state;
-        return (
-            <div className="app">
-                {Board ? <Board bar="test" /> : <p>Board is loading</p>}
-            </div>
-        )
+        const { initialData, Board } = this.state;
+        return initialData.columnOrder.map(columnId => {
+            const column = initialData.columns[columnId];
+            const tasks = column.taskIds.map(taskId => initialData.tasks[taskId]);
+
+            return column.title;
+        })
     }
 }
 
